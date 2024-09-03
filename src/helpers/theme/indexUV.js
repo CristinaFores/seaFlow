9ead6311ff5b2712a827b549da59b046cd118e73
Cristina Fores <cfores@gyoza.es>
2024-09-03 18:44:46 +0200

    Add helpers

---
+++ src/helpers/theme/indexUV.js
@@ -0,0 +1,16 @@
+const indexUV = {
+  0: { value: 0, v: 'Bajo', color: '#00FF00' },
+  1: { value: 1, v: 'Bajo', color: '#00FF00' },
+  2: { value: 2, v: 'Bajo', color: '#00FF00' },
+  3: { value: 3, v: 'Moderado', color: '#FFFF00' },
+  4: { value: 4, v: 'Moderado', color: '#FFFF00' },
+  5: { value: 5, v: 'Moderado', color: '#FFFF00' },
+  6: { value: 6, v: 'Alto', color: '#FFA500' },
+  7: { value: 7, v: 'Alto', color: '#FFA500' },
+  8: { value: 8, v: 'Muy Alto', color: '#FF4500' },
+  9: { value: 9, v: 'Muy Alto', color: '#FF4500' },
+  10: { value: 10, v: 'Extremo', color: '#FF0000' },
+  11: { value: 11, v: 'Extremo', color: '#FF0000' }
+}
+
+export default indexUV
